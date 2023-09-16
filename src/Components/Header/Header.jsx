2622c8da627fa8463310a53ee6832938540cba9c
marizal-akib <marizalakib@gmail.com>
2023-09-16 19:35:06 +0100

    adding css and fonts

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-center">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
